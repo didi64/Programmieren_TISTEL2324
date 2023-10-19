8bf6c3ca33375c3430da7778145dae4fc54ee4bf
Dieter Probst <dieter.probst@gmail.com>
2023-10-20 01:06:11 +0200

    .

--- modules/share.py
+++ modules/share.py
@@ -2,13 +2,14 @@ import pickle
 import subprocess
 from pathlib import Path
 
-NB_DIR = '/home/tistel/work'
-SC_DIR = '/home/tistel/.bin'
+ROOT = '/home/tistel/work'
+SRC = '/home/tistel/work/src'
+
 
 def init_share(pin):
-    with open(NB_DIR + '/data/.init.pkl','rb') as f:
+    with open(SRC + '/data/.init.pkl','rb') as f:
         data=pickle.load(f)
-    with open(B_DIR + '/data/emails.pkl','rb') as f:
+    with open(SRC + '/data/emails.pkl','rb') as f:
         emails=pickle.load(f)  
     
     tok = ''.join([chr((x-pin)) for x in data])
@@ -20,13 +21,13 @@ def init_share(pin):
     if user + '@edu.teko.ch' not in [x for _,_,x in emails]:
         print('Prefix {} not found!'.format(user), file=sys.stderr)
         
-    with open(NB_DIR + '/.user', 'w') as f:
+    with open(ROOT + '/.user', 'w') as f:
         f.write(user)
         
-    with open(NB_DIR  + '.token_tistel2324', 'w') as f:
+    with open(ROOT  + '.token_tistel2324', 'w') as f:
         f.write(tok)
         
-    res = subprocess.call(NB_DIR  +  './bin/init_share')
+    res = subprocess.call(ROOT + '/bin/init_share')
     if res:
         print('share=Folder fuer User {} eingerichtet.'.format(user))     
     else:
